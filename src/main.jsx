ee9d8f7ad55de4fedc998f5652d8183a6877020a
ShinadSuresh <shinadbp55@gmail.com>
2023-06-04 15:56:41 +0530

    fix: cleared some conflicts

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-=======
-import "bootstrap/dist/css/bootstrap.min.css";
->>>>>>> 1ba9cafef36e0677e6ee2eaad9f08891ce84928e
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
